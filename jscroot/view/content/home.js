6ac07508e3caf94adb040f0d9608f4f6bbb16d61
HeroBuxx <me@herobuxx.me>
2024-10-22 23:10:20 +0700

    dashbaord: home: Remove remaining big header info code
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- jscroot/view/content/home.js
+++ jscroot/view/content/home.js
@@ -36,13 +36,6 @@ export async function main() {
 function getUserFunction(result) {
   if (result.status !== 404) {
     const roundedPoin = Math.round(result.data.poin);
-    setInner("biggreet", "Halo " + result.data.name);
-    setInner(
-      "subtitle",
-      "Jumlah rollup sprint point kamu saat ini sebesar " +
-        roundedPoin +
-        " poin."
-    );
     setInner("bigpoin", roundedPoin);
   } 
   else {
