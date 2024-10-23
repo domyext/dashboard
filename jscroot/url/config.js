84358c2195ac300e8b654a3d9013c4e4ac6a2d05
agung6544 <agung.nainggolan8008@gmail.com>
2024-10-23 21:35:37 +0700

    dashboard: bring back config.js

--- jscroot/url/config.js
+++ jscroot/url/config.js
@@ -6,7 +6,7 @@ import {folderPath} from "https://cdn.jsdelivr.net/gh/jscroot/url@0.0.9/croot.js
 
 export const backend={
     user:{
-        data:"http://127.0.0.1:80/data/user",
+        data:"https://api.do.my.id/data/user",
         todo:"https://api.do.my.id/data/user/task/todo",
         doing:"https://api.do.my.id/data/user/task/doing",
         done:"https://api.do.my.id/data/user/task/done",
